544a6fa90348aaa28885a386b21f7aa41707e661
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-23 09:20:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button=document.getElementById("incrementBtn");
+let para=document.getElementById("counter");
+let val=0;
+button.addEventListener("click",()=>{
+	val++;
+	para.innertext=val;
+	alert(para);
+});
